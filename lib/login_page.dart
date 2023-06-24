ef54526f275b911f2ab2de6515c39f1695418a67
tranthedung <xdungx123@gmail.com>
2023-06-24 16:37:17 +0700

    create login page

---
+++ lib/login_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class LoginHome extends StatelessWidget {
+  const LoginHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        leading: Icon(Icons.login),
+        title: Text('Provider'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
+              ),
+              const Text(
+                'WELCOME',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'User',
+                    hintText: 'Enter User',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    filled: true,
+                    fillColor: Colors.blue.shade100,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'Password',
+                    hintText: 'Enter password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    filled: true,
+                    fillColor: Colors.blue.shade100,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30,),
+              ElevatedButton(
+                onPressed: () {
+
+                },
+                child: Text('Enter'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: const Size(100, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
